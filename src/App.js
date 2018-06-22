53828c95f5e4d07e0fdf3a0c909d79f535822233
Chen Xin <hsinchen1996@gmail.com>
2018-06-22 17:04:54 +0800

    Configuring React with bootstrap, reactstrap, react-popper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">React First Try: NavBarBrand</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+
+      /*
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +24,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      */
     );
   }
 }
